eed20a447b9f8632fa7888fdc67ab09931715260
Administrator <1015804047@qq.com>
2022-04-28 22:12:04 +0800

    Deployed 7de50e0 with MkDocs version: 1.3.0

---
+++ js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?84c90d7c8dcee9db8671a1ee9dcec54b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
